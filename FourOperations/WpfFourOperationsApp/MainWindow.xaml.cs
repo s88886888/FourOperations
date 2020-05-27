cac40cefde8387d9c884d0281685d0ac21807cc7
AKuanGG <740323865@qq.com>
2020-05-27 23:38:40 +0800

    换了个框架

---
+++ FourOperations/WpfFourOperationsApp/MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using ConsoleApp2.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfFourOperationsApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        OneTheme oneTheme = new OneTheme();
+        string[] vslist = new string[10];
+        string[] vslist1 = new string[10];
+
+        private void BtnSystem_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnL1_Click_1(object sender, RoutedEventArgs e)
+        {
+            btnadd.IsEnabled = true;
+            btnsub.IsEnabled = true;
+        }
+
+        private void btnadd_Click(object sender, RoutedEventArgs e)
+        {
+            oneTheme.AddThemeCount();
+            for (int i = 0; i < oneTheme.ArrayTheme.Count; i++)
+            {
+                vslist[i] = oneTheme.ArrayTheme[i];
+                vslist1[i] = oneTheme.ArrayAnswer[i];
+            }
+            lbxQuestion.ItemsSource = vslist;
+        }
+    }
+}
