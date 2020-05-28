9545179d0b322d1b0faeca029805a6f9ed0159c9
AKuanGG <740323865@qq.com>
2020-05-28 17:59:31 +0800

    六点钟更新

--- FourOperations/WpfFourOperationsApp/MainWindow.xaml.cs
+++ FourOperations/WpfFourOperationsApp/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using ConsoleApp2.Base;
+using ConsoleApp2.Themeup;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -25,39 +26,60 @@ namespace WpfFourOperationsApp
         {
             InitializeComponent();
         }
-        OneTheme oneTheme = new OneTheme();
+        
         string[] vslist = new string[10];
         string[] vslist1 = new string[10];
         public bool? IsChecked { get; set; }
+        //public void ToggleButton()
+        //{
+        //    return;
+        //}
         private void BtnSystem_Click(object sender, RoutedEventArgs e)
         {
 
         }
 
-        private void btnL1_Click_1(object sender, RoutedEventArgs e)
+        private void btnL1_Checked(object sender, RoutedEventArgs e)
         {
-            //btnL1.IsChecked
-            
+            btnL2.IsChecked = false;
+            btnL3.IsChecked = false;
+            btnL4.IsChecked = false;
             btnadd.IsEnabled = true;
             btnsub.IsEnabled = true;
         }
 
         private void btnadd_Click(object sender, RoutedEventArgs e)
         {
-            
-           
-            oneTheme.AddThemeCount();
-            for (int i = 0; i < oneTheme.ArrayTheme.Count; i++)
+            lbxQuestion.ItemsSource = "";
+            if (btnL1.IsChecked == true)
             {
-                vslist[i] = oneTheme.ArrayTheme[i];
-                vslist1[i] = oneTheme.ArrayAnswer[i];
+                OneTheme oneTheme = new OneTheme();
+                oneTheme.AddThemeCount();
+                for (int i = 0; i < oneTheme.ArrayTheme.Count; i++)
+                {
+                    vslist[i] = oneTheme.ArrayTheme[i];
+                    vslist1[i] = oneTheme.ArrayAnswer[i];
+                }
+                lbxQuestion.ItemsSource = vslist;
             }
-            lbxQuestion.ItemsSource = vslist;
+            else if(btnL2.IsChecked == true)
+            {
+                TwoTheme twoTheme = new TwoTheme();
+                twoTheme.AddThemeCount();
+                for (int i = 0; i < twoTheme.ArrayTheme.Count; i++)
+                {
+                    vslist[i] = twoTheme.ArrayTheme[i];
+                    vslist1[i] = twoTheme.ArrayAnswer[i];
+                }
+                lbxQuestion.ItemsSource = vslist;
+
+            }
+
         }
 
         private void btnsub_Click(object sender, RoutedEventArgs e)
         {
-
+            OneTheme oneTheme = new OneTheme();
             oneTheme.SubThemeCount();
 
             for (int i = 0; i < oneTheme.ArrayTheme.Count; i++)
@@ -66,6 +88,47 @@ namespace WpfFourOperationsApp
                 vslist1[i] = oneTheme.ArrayAnswer[i];
             }
             lbxQuestion.ItemsSource = vslist;
+        }
+
+        private void btnL2_Checked(object sender, RoutedEventArgs e)
+        {
+            btnL3.IsChecked = false;
+            btnL4.IsChecked = false;
+            btnL1.IsChecked = false;
+            btnadd.IsEnabled = true;
+            btnsub.IsEnabled = true;
+        }
+
+        private void btnL4_Checked(object sender, RoutedEventArgs e)
+        {
+            btnL2.IsChecked = false;
+            btnL3.IsChecked = false;
+            btnL1.IsChecked = false;
+            btnadd.IsEnabled = true;
+            btnsub.IsEnabled = true;
+        }
+
+        private void btnL3_Checked(object sender, RoutedEventArgs e)
+        {
+            btnL2.IsChecked = false;
+            btnL1.IsChecked = false;
+            btnL4.IsChecked = false;
+            btnadd.IsEnabled = true;
+            btnsub.IsEnabled = true;
+        }
+
+        private void btnsubmit_Click(object sender, RoutedEventArgs e)
+        {
+            if (txtAnswer1.Text == vslist1[0])
+            {
+                txtJudge.Text = "正确";
+
+            }
+             else if  (txtAnswer1.Text != vslist1[0])
+            {
+                txtJudge.Text = "×";
+               
+            }
         }
     }
 }
