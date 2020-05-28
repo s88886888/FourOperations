0423fb70d2534075caefe6dfaae91417fd8d5732
luoyu <1830902956@qq.com>
2020-05-29 01:57:07 +0800

    0.0

---
+++ FourOperations/WpfFourOperationsApp/LoginWindow1.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfFourOperationsApp
+{
+    /// <summary>
+    /// LoginWindow1.xaml 的交互逻辑
+    /// </summary>
+    public partial class LoginWindow1 : Window
+    {
+        public LoginWindow1()
+        {
+            InitializeComponent();
+        }
+        string a = "123456";
+        string b = "123";
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (text1.Text == a)
+            {
+                MainWindow mainWin = new MainWindow();
+                log.Close();
+                mainWin.Show();
+            }
+            else
+            {
+                MessageBox.Show("账号密码错误");
+            }
+
+        }
+    }
+}
